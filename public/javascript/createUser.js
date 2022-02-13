6b2dd484ef107f4587ef41b5562585f9b24c223a
DrDano <danielharned@gmail.com>
2022-02-13 10:04:30 -0800

    adding css style changes

--- public/javascript/createUser.js
+++ public/javascript/createUser.js
@@ -1,6 +1,28 @@
-async function commentFormHandler(event) {
+async function createUserHandler(event) {
     event.preventDefault();
-    let target = event.target;
-
-    console.log(target);
-}
+  
+    const comment_body = document.querySelector("textarea[name='comment-body']").value.trim();
+    const post_id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+  
+    if (comment_body) {
+      const fetchResponse = await fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify({
+          "comment_body": comment_body,
+          "post_id": post_id
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (fetchResponse.ok) {
+        console.log("ok!");
+        document.location.reload();
+      } else {
+        alert(fetchResponse.statusText);
+      }
+    }
+  }
